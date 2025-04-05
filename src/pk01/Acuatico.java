fa580f4bc26e96af71f0f4b5c95879af2b5ce02f
steven <stev3np@gmail.com>
2025-04-05 00:34:14 -0500

    Se crea clase Acuatico, que extends Vehiculo y implements Vela

---
+++ src/pk01/Acuatico.java
@@ -0,0 +1,41 @@
+package pk01;
+public class Acuatico extends Vehiculo implements Vela {
+    private String tipo;
+    private int capacidadPasajeros;
+    public Acuatico(int velocidadActual, int velocidadMaxima, String tipo, int capacidadPasajeros) {
+        super(velocidadActual, velocidadMaxima);
+        this.tipo = tipo;
+        this.capacidadPasajeros = capacidadPasajeros;
+    }
+    @Override
+    public void acelerar(int velocidad) {
+        velocidadActual += velocidad;
+        if (velocidadActual > velocidadMaxima) {
+            velocidadActual = velocidadMaxima;
+        }
+    }
+    @Override
+    public void frenar(int velocidad) {
+        velocidadActual -= velocidad;
+        if (velocidadActual < 0) {
+            velocidadActual = 0;
+        }
+    }
+    @Override
+    public int recomendarVelocidad(int velocidadViento) {
+        if (velocidadViento > 80) {
+            velocidadActual = 0;
+            System.out.println("Viento muy fuerte, no se recomienda salir.");
+        } else if (velocidadViento < 10) {
+            System.out.println("Viento muy débil, no se recomienda salir.");
+        } else {
+            System.out.println("Condiciones óptimas para navegar.");
+        }
+        return velocidadActual;
+    }
+    public void imprimirDetalles() {
+        imprimir();
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Capacidad de Pasajeros: " + capacidadPasajeros);
+    }
+}
