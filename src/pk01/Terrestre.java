5b023a99745f7d97ed900fb403bc68734c3bf68f
Juan Camilo <jcamilomc21@gmail.com>
2025-04-04 23:34:33 -0500

    Se realizo correcciones en el clase Vehiculo y Terrestre, tambien se crea las dos interfaces Motor y Vela

--- src/pk01/Terrestre.java
+++ src/pk01/Terrestre.java
@@ -1,10 +1,39 @@
 package pk01;
-public class Terrestre extends Vehiculo {
-    public Terrestre(int velocidadActual, int velocidadMaxima) {
+public class Terrestre extends Vehiculo implements Motor {
+    private int llantas;
+    private String usoVehiculo;
+
+    public Terrestre(int velocidadActual, int velocidadMaxima, int llantas, String usoVehiculo) {
         super(velocidadActual, velocidadMaxima);
+        this.llantas = llantas;
+        this.usoVehiculo = usoVehiculo;
     }
+
+    @Override
+    public void acelerar(int velocidad) {
+        velocidadActual += velocidad;
+        if (velocidadActual > velocidadMaxima) {
+            velocidadActual = velocidadMaxima;
+        }
+    }
+
+    @Override
+    public void frenar(int velocidad) {
+        velocidadActual -= velocidad;
+        if (velocidadActual < 0) {
+            velocidadActual = 0;
+        }
+    }
+
+    @Override
     public int calcularRevolucionesMotor(int fuerza, int radio) {
         return fuerza * radio;
+    }
+
+    public void imprimirDetalles() {
+        imprimir();
+        System.out.println("Llantas: " + llantas);
+        System.out.println("Uso: " + usoVehiculo);
     }
 }
 
--- src/pk01/Vehiculo.java
+++ src/pk01/Vehiculo.java
@@ -1,25 +1,19 @@
 package pk01;
-public class Vehiculo {
-     protected int velocidadActual;
+abstract class Vehiculo {
+    protected int velocidadActual;
     protected int velocidadMaxima;
+
     public Vehiculo(int velocidadActual, int velocidadMaxima) {
         this.velocidadActual = velocidadActual;
         this.velocidadMaxima = velocidadMaxima;
     }
+
     public void imprimir() {
-        System.out.println("Velocidad Actual: " + velocidadActual + ", Velocidad Máxima: " + velocidadMaxima);
+        System.out.println("Velocidad Actual: " + velocidadActual + " km/h");
+        System.out.println("Velocidad Máxima: " + velocidadMaxima + " km/h");
     }
-    public void acelerar(int velocidad) {
-        velocidadActual += velocidad;
-        if (velocidadActual > velocidadMaxima) {
-            velocidadActual = velocidadMaxima;
-        }
-    }
-    public void frenar(int velocidad) {
-        velocidadActual -= velocidad;
-        if (velocidadActual < 0) {
-            velocidadActual = 0;
-        }
-    }
+
+    public abstract void acelerar(int velocidad);
+    public abstract void frenar(int velocidad);
 }
 
